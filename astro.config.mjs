208a9803f7d52ce74084c12470e7844dd329a5e7
Jonathan Libonati (V) <jlibonati@apple.com>
2026-03-12 17:16:29 -0300

    updating config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,18 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({
+/* export default defineConfig({
   site: 'https://nahuelhuapi4265.com',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [sitemap()]
+});
+ */
+
+export default defineConfig({
+  site: 'https://nahuelHuapiWeb.github.io/nahuelHuapiWeb',
+  base: '/nahuelHuapiWeb',
   vite: {
     plugins: [tailwindcss()]
   },
